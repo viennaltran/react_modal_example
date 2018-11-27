d58325d8a9b40b41c8f1f6332ae4cc742185b99b
viennaltran <lanvienhtran@gmail.com>
2018-11-27 11:26:52 -0800

    testing

--- src/components/basic_modal/modal.js
+++ src/components/basic_modal/modal.js
@@ -7,7 +7,7 @@ class ButtonModal extends Component {
     };
 
     open = () => this.setState({isOpen: true});
-
+    
     close = () => this.setState({isOpen: false});
 
     render(){
@@ -17,15 +17,36 @@ class ButtonModal extends Component {
                 <div className="basic-modal" onClick={this.close}>
                     <div onClick={e => e.stopPropagation()} className="basic-modal-content">
                         <div onClick={this.close} className="basic-modal-close">X</div>
-                        <h1>Basic Modal Example</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis fugiat laboriosam quo? Ab officia tempore ratione id, modi possimus adipisci autem, inventore reiciendis facere nesciunt.</p>
+                            <div>
+                                <form className="col s12">
+                                <div className="row">
+                                <div className="input-field col s12">
+                                <h5 ref={subtitle1 => this.subtitle1 = subtitle1}>Enter Category</h5>
+                                <textarea onChange={ e => this.setState({term: e.target.value})}  className="materialize-textarea" id="textarea1"></textarea>
+                                </div>
+                                </div>
+
+                                <div className="row"> 
+                                <div className="input-field col s12">
+                                <h5 ref={subtitle2 => this.subtitle2 = subtitle2}>Enter Title</h5>
+                                <textarea onChange={ e => this.setState({term: e.target.value})}  className="materialize-textarea" id="textarea1"></textarea>
+                                </div>  
+                                </div>
+
+                                <div className = "buttonDiv">
+                    <div to = "/createflashcards" className="green lighten-2 btn waves-effect waves-light btn-large" type="done" name="action">
+                    Create Card
+                    </div>
+                </div>
+                                </form>
+                            </div>
                     </div>
                 </div>
             )
         }
 
         return (
-            <button onClick={this.open} className="btn btn-outline-primary">Open Button Modal</button>
+            <button onClick={this.open} className="btn btn-outline-primary">Create Category</button>
         );
     }
 }
